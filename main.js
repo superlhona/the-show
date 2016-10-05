5f5f67df220c5c226a74c26df0bca294e8eb26c5
illona fedora <superlhona@gmail.com>
2016-10-05 22:57:07 +0200

    correcting flow and storyline. animation is now a mix of queued animation and on event animation.

--- main.js
+++ main.js
@@ -1,29 +1,82 @@
 $(document).ready(function(){
 
-	$( "#button-0" ).click(function(){
+	$(function(){
 		$(".element").typed({
 			strings: ["Kids..", "I'm going to tell you an incredible story.", "The story of how I met your mother..", "Click here to continue!"],
 			typeSpeed: 0
 		});
-		$( "button" ).hide( "fast" );
 	});
 
 	$( ".element" ).click(function() {
-	  $( "#ted-1" ).show( "slow" );
-	  $( ".two" ).hide( "fast" );
-	});
+  	  $( "#tedmosby" ).show( "slow" );
+ 	  $( ".one" ).hide( "fast" ).queue( function() {
+        $( "#intro" ).show( 2000 );
+ 
+        // This tells jQuery to continue to the next item in the queue
+        $( this ).dequeue();
+    } );
 
-	$("#ted-1").click(function() {
-   		$( "#gf-1" ).fadeIn( "slow" ), function() {
-   			$("#intro-vic").show( "fast" );
-   		}
-	});
+  	});
 
-	$("#ted-1").click(function() {
-   		$( "#gf-1" ).fadeIn( "fast" );
-		$("#intro-vic").slideDown( "slow" );
-   		}
-	});
+
+// $( "#tedmosby" )
+//     .animate( {
+//         height: 150
+//     }, "slow")
+//     .queue( function() {
+//         $( "#intro" ).fadeIn( "slow" );
+ 
+//         // This tells jQuery to continue to the next item in the queue
+//         $( this ).dequeue();
+//     } );
+
+
+
+
+
+	// $(".tedmosby").click(function(){
+ //        var div = $("#intro");
+ //        div.animate({height: 300}, "slow");
+ //        div.animate({width: 300}, "slow");
+ //        div.animate({height: 100}, "slow");
+ //        div.animate({width: 100}, "slow");
+	// });
+
+	// $( ".element" ).click(function() {
+	//   	$( "#tedmosby" ).show( "slow" );
+	//   	$( ".one" ).hide( "fast" );
+	//   	.queue(function() {
+ //      $( this ).show( "#intro" ).dequeue();
+ //    })
+	// });
+
+// 	$( document.body ).click(function() {
+//   $( "div" )
+//     .show( "slow" )
+//     .animate({ left: "+=200" }, 2000 )
+//     .queue(function() {
+//       $( this ).addClass( "newcolor" ).dequeue();
+//     })
+//     .animate({ left: "-=200" }, 500 )
+//     .queue(function() {
+//       $( this ).removeClass( "newcolor" ).dequeue();
+//     })
+//     .slideUp();
+// });
+
+
+
+// 	$("#ted-1").click(function() {
+//    		$( "#gf-1" ).fadeIn( "slow" ), function() {
+//    			$("#intro-vic").show( "fast" );
+//    		}
+// 	});
+
+// 	$("#ted-1").click(function() {
+//    		$( "#gf-1" ).fadeIn( "fast" );
+// 		$("#intro-vic").slideDown( "slow" );
+//    		}
+// 	});
 
 	// $(function(){
  //      $("#intro-vic").appear("fast");
